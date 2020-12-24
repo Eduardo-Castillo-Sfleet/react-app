0da8ddccb61e7e64c37370e8b26b70f7aa7c3ec3
Eduardo-Castillo-Sfleet <eduardo.castillo@sfleet.mx>
2020-12-23 20:51:24 -0600

    manejando los target en form

--- src/components/DatoForm.js
+++ src/components/DatoForm.js
@@ -2,10 +2,33 @@ import React from 'react';
 
 class DatoForm extends React.Component{
 
+    state = {
+        title: '',
+        descripcion: ''
+    }
+
+    formEnviado = (e) => {
+        e.preventDefault()
+    }
+
+    onChange = e => {
+        console.log(e.target.name, e.target.value)
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+        console.log(this.state.title)
+    }
+
     render() {
         return (
-            <form>
-                <input type="text" placeholder="Escribe el dato"/>
+            <form onSubmit={this.formEnviado}>
+                <input type="text" placeholder="Escribe el dato" name="title" onChange={this.onChange} value={this.state.title}/>
+                <br/>
+                <br/>
+                <textarea placeholder="Descripcion"></textarea>
+                <br/>
+                <br/>
+                <input type="submit" value="Guardar"/>
             </form>
         )
     }
